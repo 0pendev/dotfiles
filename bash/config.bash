9d9a4832f59ff5d3c1a0344b31492b5ac6b7b20e
Francisco Freitas <opendev@linux-lv1o.home>
2019-02-13 20:46:12 +0100

    Cleaned bash config

--- bash/config.bash
+++ bash/config.bash
@@ -1,7 +1,3 @@
-# Added transfer command to easily share a file (see transfer.sh)
-transfer() { if [ $# -eq 0 ]; then echo -e "No arguments specified. Usage:\necho transfer /tmp/test.md\ncat /tmp/test.md | transfer test.md"; return 1; fi
-tmpfile=$( mktemp -t transferXXX ); if tty -s; then basefile=$(basename "$1" | sed -e 's/[^a-zA-Z0-9._-]/-/g'); curl --progress-bar --upload-file "$1" "https://transfer.sh/$basefile" >> $tmpfile; else curl --progress-bar --upload-file "-" "https://transfer.sh/$1" >> $tmpfile ; fi; cat $tmpfile; rm -f $tmpfile; } 
-
 # Export configuration for my org-files
 org2pdf(){
     pandoc $1 -o $2 --template $3 --number-sections --listings
@@ -22,20 +18,17 @@ org2ensibs(){
     unlink ./logo.png
 }
 
-# Start screen on startup if $SCREEN is defined
-if [[ $SCREEN ]];then
-    if [[ -z "$STY" ]]; then
-	screen -xRR session_name
-    fi
-fi
-
 #Console output
 #export PS1="╭─ [\[\e[36m\]\u\[\e[m\]\[\e[36m\]@\[\e[m\]\[\e[36m\]\h\[\e[m\] \[\e[35m\]\W\[\e[m\]] \$(__git_ps1 '(%s)')\n╰─── "
 
 # Golang configuration
-export PATH=$PATH:/usr/local/go/bin
-export GOPATH=~/go
-export PATH=$PATH:$GOPATH/bin
+export DEFAULT_PATH = $PATH
+setgopath(){
+	export GOPATH=$1
+	export PATH=$DEFAULT_PATH:$GOPATH/bin
+}
+
+setgopath ~/GO_WORKSPACES/DEFAULT
 
 # Pip binary path
 export PATH=$PATH:$HOME/.local/bin
