3bd3e7831e8c5b5348c108a19cd0548749f0941a
0pendev <contact@ffreitas.io>
2019-05-04 10:25:42 +0200

    Updated install script

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 sudo zypper dup
-sudo zypper in papirus-icon-theme breeze5-cursors metatheme-adapta-common wget chromium emacs i3 rofi sakura i3status git zsh
+sudo zypper in\
+ papirus-icon-theme breeze5-cursors metatheme-adapta-common\
+ wget chromium emacs i3 rofi sakura i3status git zsh\
+ gnome-font-viewer lxappearance lxrandr nm-applet\
+ bluetoothctl
 mkdir ~/Documents
 cd ~/Documents
 git clone http://github.com/0pendev/dotfiles
@@ -13,3 +17,7 @@ ln -s ~/Documents/dotfiles/rofi ~/.config
 ln -s ~/Documents/dotfiles/sakura ~/.config
 ln -s ~/Documents/dotfiles/xorg/.xinitrc ~/.xinitrc
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+mkdir ~/Downloads
+cd ~/Downloads
+git clone https://github.com/domtronn/all-the-icons.el
+echo "Now install the fonts and configure the theme."
