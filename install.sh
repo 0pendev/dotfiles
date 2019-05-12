9ffc6de7c600a5d7d0d3a1c4c7aaec09f0d98334
0pendev <contact@ffreitas.io>
2019-05-12 20:41:03 +0200

    Updated installation script

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 sudo zypper dup
-sudo zypper in\
+sudo zypper in -y\
  papirus-icon-theme breeze5-cursors metatheme-adapta-common\
  wget chromium emacs i3 rofi sakura i3status git zsh\
  gnome-font-viewer lxappearance lxrandr thunar\
  bluez xinit xorg-x11 xorg-x11-server htop keepassxc\
- telegram-desktop podman buildah python3-virtualenv
+ telegram-desktop podman buildah python3-virtualenv\
+ go
 sudo zypper in -t pattern fonts
 mkdir ~/Documents
 cd ~/Documents
