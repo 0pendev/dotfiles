91c56e1e896b2308d3d5e9ce1995a81b09ca96ff
0pendev <contact@ffreitas.io>
2019-05-14 15:47:10 +0200

    Updated installation script.

--- install.sh
+++ install.sh
@@ -6,7 +6,8 @@ sudo zypper in\
  gnome-font-viewer lxappearance lxrandr thunar\
  bluez xinit xorg-x11 xorg-x11-server htop keepassxc\
  telegram-desktop podman buildah python3-virtualenv\
- go xfce4-power-manager powertop xfce4-screenshooter 
+ go xfce4-power-manager powertop xfce4-screenshooter\
+ lxsession 
 sudo zypper in -t pattern fonts
 sudo systemctl enable powertop
 sudo systemctl start powertop
