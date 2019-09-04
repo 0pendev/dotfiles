3d32c65c60cc3de551570c6e2bf45c27c942073d
0pendev <contact@ffreitas.io>
2019-09-04 09:38:15 +0200

    updated installation script

--- install.sh
+++ install.sh
@@ -9,29 +9,38 @@ warning(){
 
 
 log "[+] Starting installation $(date)"
+
 log "Updating system"
 sudo zypper dup
+
 log "Installing wayland graphical environment"
 sudo zypper in\
      rofi sakura thunar sway evince brightnessctl swaylock lxsession lxappearance
+
 log "Installing X.org graphical environment"
 sudo zypper in\
      i3 i3status lxrandr xinit xorg-x11 xorg-x11-server xfce4-screenshooter xfce4-power-manager 
+
 log "Installing development tools"
 sudo zypper in\
      wget emacs git zsh htop podman buildah python3-virtualenv go 
+
 log "Installing theme assets"
 sudo zypper in\
      paper-icon-theme metatheme-paper-common breeze5-cursors\
      metatheme-greybird-geeko-common elementary-xfce-icon-theme
+
 log "Installing usefull stuff"
 sudo zypper in\
      chromium gnome-font-viewer keepassxc telegram-desktop powertop
+
 log "Installing fonts"
 sudo zypper in -t pattern fonts
+
 log "Setting up powertop"
 sudo systemctl enable powertop
 sudo systemctl start powertop
+
 log "Setting up dotfiles"
 mkdir ~/Documents
 cd ~/Documents
@@ -44,16 +53,19 @@ ln -s ~/Documents/dotfiles/rofi ~/.config
 ln -s ~/Documents/dotfiles/sway ~/.config
 ln -s ~/Documents/dotfiles/sakura ~/.config
 ln -s ~/Documents/dotfiles/xorg/.xinitrc ~/.xinitrc
+
 log "Downloading all-the-icons"
 mkdir ~/Downloads
 cd ~/Downloads
 git clone https://github.com/domtronn/all-the-icons.el
 warning "Now install the fonts and configure the theme."
+
 log "Setting up git globally"
 git config --global user.email "contact@ffreitas.io"
 git config --global user.name "0pendev"
+
 log "Setting up hostname to r0nd"
 sudo log "r0nd" > /etc/hostname
-log "Installing zsh"
-sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
+log "Installing oh-my-zsh"
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
