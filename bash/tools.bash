4db10b38baed4e0f91e3e77636791588fa218115
0pendev <contact@ffreitas.io>
2019-09-25 23:39:11 +0200

    Updated radare2 script

--- bash/tools.bash
+++ bash/tools.bash
@@ -1,15 +1,23 @@
 #!/bin/bash
 # Radare2 framework
-r2(){
+r2env(){
+    current=$(pwd)/$1
+    r2current=/home/r2/shared
     home=~
+    r2home=/home/r2
     radare2=$home/.local/share/radare2
-    projects=$radare2
+    r2radare2=$r2home/.local/share/radare2
+    projects=$radare2/projects
+    r2projects=$r2radare2/projects
     if ! [ -d "$radare2" ];then
 	mkdir $radare2
     fi
     if ! [ -d "$projects" ];then
 	mkdir $projects
     fi
-    sudo podman run --rm -v $home/.local/share/radare2/projects:/home/r2/.local/share/radare2/projects -v $1:/home/r2/current -it 0pendev/radare2 bash
+    sudo podman run --rm \
+	-v $projects:$r2projects \
+	-v $current:$r2current \
+	-it 0pendev/radare2 bash
 }
 
