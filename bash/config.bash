c6e1a0a016b502c2274ef8b61a9e49ddeb76c5b2
Kn0wl3dge <baptistin.boilot@epita.fr>
2021-06-19 22:20:28 +0200

    Cleaned bash configuration
    Removed the startup script from config.bash and moved usefull functions to tools.bash

--- bash/config.bash
+++ bash/config.bash
@@ -1,35 +1,5 @@
-# Screen configuration
-if [ "$(tty)" = "/dev/tty1" ]; then
-        exec sway
-fi
-
-if [ "$(tty)" = "/dev/tty2" ]; then
-        exec startx
-fi
-
-printshellcode() {
-    objdump -d $1 |grep '[0-9a-f]:'|grep -v 'file'|cut -f2 -d:|cut -f1-6 -d' '|tr -s ' '|tr '\t' ' '|sed 's/ $//g'|sed 's/ /\\x/g'|paste -d '' -s |sed 's/^/"/'|sed 's/$/"/g'
-}
-
-# Export configuration for my org-files
-org2pdf(){
-    pandoc $1 -o $2 --template $3 --number-sections --listings
-}
-
-org2ensibs(){
-    echo "[+] Starting org2pdf"
-    echo "[+] Linking logo.png"
-    ln -s ~/.pandoc/ressources/logo.png ./logo.png
-    echo "[+] Crating temporary file __report.org"
-    cat ~/.pandoc/ressources/eisvogel.headers > __report.org
-    cat $1.org >> __report.org
-    echo "[+] Generating PDF"
-    org2pdf __report.org $1.pdf eisvogel
-    echo "[+] Removing temporary file __report.org"
-    rm __report.org
-    echo "[+] Unlinking logo.png"
-    unlink ./logo.png
-}
+# Sets emacs binary to point to flatpak
+export EMACS="/usr/bin/flatpak run org.gnu.emacs"
 
 # Pip binary path
 export PATH=$PATH:$HOME/.local/bin
--- bash/tools.bash
+++ bash/tools.bash
@@ -1,4 +1,29 @@
 #!/bin/bash
+
+printshellcode() {
+    objdump -d $1 |grep '[0-9a-f]:'|grep -v 'file'|cut -f2 -d:|cut -f1-6 -d' '|tr -s ' '|tr '\t' ' '|sed 's/ $//g'|sed 's/ /\\x/g'|paste -d '' -s |sed 's/^/"/'|sed 's/$/"/g'
+}
+
+# Export configuration for my org-files
+org2pdf(){
+    pandoc $1 -o $2 --template $3 --number-sections --listings
+}
+
+org2ensibs(){
+    echo "[+] Starting org2pdf"
+    echo "[+] Linking logo.png"
+    ln -s ~/.pandoc/ressources/logo.png ./logo.png
+    echo "[+] Crating temporary file __report.org"
+    cat ~/.pandoc/ressources/eisvogel.headers > __report.org
+    cat $1.org >> __report.org
+    echo "[+] Generating PDF"
+    org2pdf __report.org $1.pdf eisvogel
+    echo "[+] Removing temporary file __report.org"
+    rm __report.org
+    echo "[+] Unlinking logo.png"
+    unlink ./logo.png
+}
+
 # Radare2 framework
 r2env(){
     current=$(pwd)/$1
